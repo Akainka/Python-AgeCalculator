a149ba6b897c7303ad3b355f16c258237933ca71
Akainka <36900981+Akainka@users.noreply.github.com>
2019-04-20 13:00:51 +0200

    Add files via upload

---
+++ AgeCalculator.py
@@ -0,0 +1,24 @@
+# AgeCalculator
+
+# DO ZROBIENIA:
+# # POKAŻ BŁĄD JEŚLI WIEK JEST UJEMNY
+
+
+def AgeCalculator():
+    while True:
+        year = input("Enter a present year: ")
+        try:
+            year = int(year)
+            break
+        except ValueError:
+            print("Only integers allowed")
+    while True:
+        birth = input("Enter the year of your birth: ")
+        try:
+            birth = int(birth)
+            break
+        except ValueError:
+            print("Only integers allowed")
+    age = year - birth
+    print(f"You are {age} years old.")
+AgeCalculator()
